b3d5ec91dd17ce800b052362307172eb007effb2
Mateus Figueiredo <mateusmelosf@gmail.com>
2025-09-21 14:42:36 -0300

    Fix validation formatting in Office model

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,4 +1,10 @@
 class Office < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+
   has_many :memberships
   has_many :users, throught: :membershipsra
 end
