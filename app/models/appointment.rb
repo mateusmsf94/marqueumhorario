1f2d86b05105e64a4d7eb243753b37b99d6ed5ef
Mateus Figueiredo <mateusmelosf@gmail.com>
2025-10-11 00:27:58 +0000

    Add appointment scopes and booking validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -12,6 +12,14 @@ class Appointment < ApplicationRecord
   # Custom datetime validations
   validate :end_after_start
   validate :book_before_start
+  validate :start_in_future, on: :create
+  validate :prevent_double_booking
+
+  scope :upcoming, -> { where("start_datetime >= ?", Time.current).order(:start_datetime) }
+  scope :past, -> { where("start_datetime < ?", Time.current).order(start_datetime: :desc) }
+  scope :by_date, ->(date) { where(start_datetime: date.beginning_of_day..date.end_of_day).order(:start_datetime) }
+  scope :for_user, ->(user) { where("provider_id = :user_id OR customer_id = :user_id", user_id: user.is_a?(User) ? user.id : user) }
+  scope :for_office, ->(office) { where(office_id: office.is_a?(Office) ? office.id : office) }
 
   private
 
@@ -28,6 +36,35 @@ class Appointment < ApplicationRecord
 
     if book_datetime > start_datetime
       errors.add(:book_datetime, "must be before or at start datetime")
+    end
+  end
+
+  def start_in_future
+    return if start_datetime.blank?
+
+    if start_datetime < Time.current
+      errors.add(:start_datetime, "must be in the future")
+    end
+  end
+
+  def prevent_double_booking
+    return if start_datetime.blank? || end_datetime.blank? || provider_id.blank? || office_id.blank? || customer_id.blank?
+
+    overlapping_scope = self.class
+      .where.not(id: id)
+      .where("start_datetime < :end_time AND end_datetime > :start_time", end_time: end_datetime, start_time: start_datetime)
+      .where.not(status: self.class.statuses[:cancelled])
+
+    if overlapping_scope.where(provider_id: provider_id).exists?
+      errors.add(:base, "provider is already booked during this time")
+    end
+
+    if overlapping_scope.where(customer_id: customer_id).exists?
+      errors.add(:base, "customer is already booked during this time")
+    end
+
+    if overlapping_scope.where(office_id: office_id).exists?
+      errors.add(:base, "office is already booked during this time")
     end
   end
 end
